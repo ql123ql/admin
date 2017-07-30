a0a0185bbda4155ba07bea744b2c05a26aac11f9
ql123ql <714849334@qq.com>
2017-07-30 23:58:23 +0800

    1

--- backend/views/rbac/permission-index.php
+++ backend/views/rbac/permission-index.php
@@ -16,8 +16,8 @@ echo \yii\bootstrap\Html::a('添加',['add-permission'],['class'=>'btn btn-info'
             <td><?=$model->name?></td>
             <td><?=$model->description?></td>
             <td>
-                <?=\yii\bootstrap\Html::a('删除',['rbac/del-permission','name'=>$model->name],['class'=>'btn btn-danger btn-xs'])?>
                 <?=\yii\bootstrap\Html::a('修改',['rbac/edit-permission','name'=>$model->name],['class'=>'btn btn-info btn-xs'])?>
+                <?=\yii\bootstrap\Html::a('删除',['rbac/del-permission','name'=>$model->name],['class'=>'btn btn-danger btn-xs'])?>
             </td>
         </tr>
     <?php endforeach;?>
